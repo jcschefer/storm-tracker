c741ec0181ad15618bf46366e0d87c87e76e58cd
Jack Schefer <jack.schefer9@gmail.com>
2017-01-27 15:43:12 -0500

    export line drawing config to global variable

--- builder.js
+++ builder.js
@@ -1,4 +1,5 @@
-var DRAW_LINES = true;
+var DRAW_LINES = true;  // draw the path while the storm is moving
+var KEEP_LINES = false;  // keep the lines once the storm has died out
 
 function update( json, map, markers, begin) {
    // 1. find the storms we need to display
@@ -24,6 +25,10 @@ function update( json, map, markers, begin) {
       }
       if( !contains )
       {
+         if( !KEEP_LINES )
+         {
+            markers[m].line.setPath([]);
+         }
          markers[m].mark.setMap(null);
          delete markers[m];
       }
@@ -43,20 +48,22 @@ function update( json, map, markers, begin) {
                      url: 'icon.svg'
                   }
                }),
+            line: new google.maps.Polyline({
+               path: [],
+               strokeColor: '#FF0000',
+               strokeOpacity: 1.0,
+               strokeWeight: 2,
+               map:map
+            }),
             storm: current[i].storm
          } 
       }
       else if( DRAW_LINES )
       {
-         var from = markers[i].mark.position;
-         var to   = new google.maps.LatLng(current[i].lat, current[i].lon);
-         var line = new google.maps.Polyline({
-            path: [from, to],
-            strokeColor: '#FF0000',
-            strokeOpacity: 1.0,
-            strokeWeight: 2,
-            map:map
-         });
+         var pos = new google.maps.LatLng(current[i].lat, current[i].lon);
+         var newp = markers[i].line.getPath();
+         newp.push(pos);
+         markers[i].line.setPath(newp);
       }
    
       markers[i].mark.setPosition( new google.maps.LatLng( current[i].lat, current[i].lon ));
