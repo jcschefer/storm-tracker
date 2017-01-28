a71a876e1c812558db803b9a7e2de45d83efec40
Jack Schefer <jack.schefer9@gmail.com>
2017-01-27 21:03:02 -0500

    update parser_time.py to work for all areas

--- parser_time.py
+++ parser_time.py
@@ -23,11 +23,16 @@ def parse_by_time(path):
     sequence = []
     with open('raw_data/' + path, 'r') as f:
         for line in f:
+            line = line.replace('NOT NAMED', 'NOT_NAMED')
+            line = line.replace('IONE 1', 'IONE_1')
+            line = line.replace('IONE 2', 'IONE_2')
             cols = line.split()
+            cols = list(filter(None, cols))
             date = datetime(year=int(cols[0]), month=int(cols[1]), day=int(cols[2]), hour=int(cols[3]))
             # date = cols[0] + '/' + cols[1] + '/' + cols[2] + ':' + cols[3]
-            lon = 360.0 - float(cols[8])
-            sequence.append({'time': date, 'lat': float(cols[7]), 'lon': int(lon * 100 + 0.5) / 100.0, 'speed': int(cols[9]), 'storm': int(cols[4])})
+            lon = 360.0 - float(cols[7])
+            try: sequence.append({'time': date, 'lat': float(cols[6]), 'lon': int(lon * 100 + 0.5) / 100.0, 'speed': int(cols[8]), 'storm': path.replace('.data', '') + cols[4] })
+            except ValueError: print(line)
     sequence.sort(key=lambda a: a['time'])
     return sequence
 
